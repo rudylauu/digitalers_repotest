27926c042d0f99adb8d15c662f268d96c214f9ec
Rudy-Lau <rudylauu@gmail.com>
2021-10-13 22:12:58 -0300

    practica sql

---
+++ Clase21 SQL/Consultas instnwnd.sql
@@ -0,0 +1,9 @@
+select 
+	[CategoryID],
+	[CategoryName],
+	[Description]
+	from Categories
+ORDER BY CategoryName DESC; --ordena descendiente
+
+SELECT * FROM Employees
+ORDER BY FirstName, TitleOfCourtesy; --ORDEN DE DOS COLUMNAS, ORDENA LA PRIM ERA QUE ESCRIBI 
