0768151301d5575eaf2614fe4da77f0f9c4dc75c
Rudy-Lau <rudylauu@gmail.com>
2021-10-18 22:03:10 -0300

    .

--- Clase21 SQL/Consultas instnwnd.sql
+++ Clase21 SQL/Consultas instnwnd.sql
@@ -6,4 +6,8 @@ select
 ORDER BY CategoryName DESC; --ordena descendiente
 
 SELECT * FROM Employees
-ORDER BY FirstName, TitleOfCourtesy; --ORDEN DE DOS COLUMNAS, ORDENA LA PRIM ERA QUE ESCRIBI 
+ORDER BY FirstName, TitleOfCourtesy; --ORDEN DE DOS COLUMNAS, LE DA PRIORIDAD A LA PRIMERA QUE ESCRIBI
+
+SELECT * FROM Customers
+where Country = 'USA' AND (Region = 'OR' OR Region= 'WA') 
+order by CompanyName asc
