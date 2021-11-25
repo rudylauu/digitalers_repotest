2746b17f9a001e510c7c98f7db346aad988d7541
Rudy-Lau <rudylauu@gmail.com>
2021-11-24 22:24:21 -0300

    clases genericas

--- clase27/02clasesGenericas/clasesGenericas/Program.cs
+++ clase27/02clasesGenericas/clasesGenericas/Program.cs
@@ -10,6 +10,18 @@ namespace clasesGenericas
     {
         static void Main(string[] args)
         {
+            miClaseGenerica<string> miObjetoGenerico = new miClaseGenerica<string>();
+            string valor = "Hola Soy Rudy";
+            miObjetoGenerico.Mostrar(valor);
+            miObjetoGenerico.OtroMostrar<string>(valor);
+
+            miClaseGenerica<DateTime> miOtroObjetoGenerico = new miClaseGenerica<DateTime>();
+            DateTime ahora = DateTime.Now;
+            miOtroObjetoGenerico.Mostrar(ahora);
+            miOtroObjetoGenerico.OtroMostrar<DateTime>(ahora);// o puedo colocar <string>("hola"), ya que no depende de del tipo de dato de la clase.
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ clase27/02clasesGenericas/clasesGenericas/miClaseGenerica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesGenericas
+{
+    class miClaseGenerica<T>
+    {
+        public void Mostrar(T parametro) //parametro de la clase
+        {
+            Console.WriteLine(parametro.GetType().Name);// GetType = obtengo tipo de dato y .Name= le pido el nombre
+        }
+
+        public void OtroMostrar<T2Local>(T2Local parametro) //parametro del metodo
+        {
+            Console.WriteLine(parametro.GetType().Name);
+        }
+    }
+}
