ca51e96c7fde6abd64d569a004b2d75ef64a2dbf
Rudy-Lau <rudylauu@gmail.com>
2021-11-25 20:28:40 -0300

    Metodos de extension

---
+++ clase27/03MetodosDeExtension/MetodosDeExtension/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDeExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string miTexto = "Guatemala";
+            Console.WriteLine(miTexto);
+
+            miTexto = miTexto.Invertir();//invierte y se guarda en miTexto
+            Console.WriteLine(miTexto);
+
+            Console.ReadKey();//espera a que se presione una tecla
+        }
+    }
+}
---
+++ clase27/03MetodosDeExtension/MetodosDeExtension/stringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDeExtension
+{
+    public static class stringExtensions
+    {
+        //cuando se llame a este metodo se va a llamar como:
+        //variable.Invertir()
+        //y "self" va a corresponder como "variable"
+
+        public static string Invertir(this string self) 
+        {
+            char[] array = self.ToCharArray(); //convierto a Array
+            Array.Reverse(array);  // revertir el array
+            return new string(array); //retorno el nuevo string
+        }
+    }
+}
