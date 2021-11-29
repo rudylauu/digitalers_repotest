2550c9b666dd0278752a24a52e0eaedfccd23543
Rudy-Lau <rudylauu@gmail.com>
2021-11-29 12:26:45 -0300

    Delegados

---
+++ clase28/01Delegados/Delegados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    class Program
+    {
+        delegate void Escribir(string texto);
+        static void Main(string[] args)
+        {
+            Escribir escritor = Console.WriteLine;
+            escritor("Hola Mundo");
+
+            Saludar(escritor); //llama a la funcion Saludar
+            // es lo mismo que Saludar(Console.WriteLine);
+
+
+            Console.ReadKey();
+        }
+
+        static void Saludar(Escribir unEscritor) 
+        {
+            unEscritor("Hola mundo desde la funcion Saludar");
+        }
+    }
+}
