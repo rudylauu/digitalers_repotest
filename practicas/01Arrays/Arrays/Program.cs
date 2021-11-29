de2e83b5dbac70df7204a5fb46915a6edd4d8392
Rudy-Lau <rudylauu@gmail.com>
2021-11-29 12:27:17 -0300

    practica arrays

---
+++ practicas/01Arrays/Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string[] vectorPersonas = new string[] {"Juan","Pedro","luis","maria"};
+            for (int i = 1; i > 4; i++)
+            {
+
+                Console.WriteLine("Persona en la posición { 0}: { 1}", i, vectorPersonas[i]);
+            }*/
+           
+
+        }
+
+    }
+}
